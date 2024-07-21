8e40ba701ca1b7e538216a00e36da4c39e257bdd
Qishuai <ferdinandzhong@gmail.com>
2024-07-21 13:20:12 +0800

    reduce each sample's size

--- research/qwen_finetune_cmd.sh
+++ research/qwen_finetune_cmd.sh
@@ -2,15 +2,17 @@ export
 
 python punctuator/training/llm_finetune.py --model_name Qwen/Qwen2-1.5B-Instruct \
     --tokenizer_name Qwen/Qwen2-1.5B-Instruct \
-    --dataset_dir "data/llm_dtasets/special_token" \
+    --dataset_dir "data/llm_datasets/special_token" \
     --model_type QWEN2 \
     --additional_special_tokens "{\"additional_special_tokens\": [\"@P\"]}" \
     --additional_tokinezer_config "{\"padding_side\": \"left\", \"truncation_side\": \"left\"}" \
-    --deepspeed "research/qwen_finetune_dp_config.json" \
-    --report_to runs/llm_finetune/qwen2/special_token \
+    --deepspped  research/qwen_finetune_dp_config.json \
+    --report_to tensorboard \
+    --logging_dir runs/llm_finetune/qwen2/special_token \
     --output_dir models/llm_finetune/qwen2/special_token \
     --load_best_model_at_end \
-    --eval_strategy epoch \
-    --max_steps 50000 \
-    --batch_eval_metrics
-
+    --eval_strategy steps \
+    --save_strategy steps \
+    --num_ 50000 \
+    --batch_eval_metrics \
+    
