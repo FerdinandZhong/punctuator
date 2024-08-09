4cf515fc0c823fa243a8dc00da9f00c228016c1c
Qishuai <ferdinandzhong@gmail.com>
2024-08-09 21:44:58 +0800

    add evaluation entry script

---
+++ research/evaluation_step2.py
@@ -0,0 +1,26 @@
+import argparse
+
+from punctuator.training.evaluation_step2 import EvaluationArguments, EvaluationPipeline
+
+parser = argparse.ArgumentParser()
+parser = EvaluationArguments.add_cli_args(parser)
+evaluation_args = parser.parse_args()
+(
+    evaluation_corpus, 
+    evaluation_tags, 
+    evaluation_step1_features,
+    label2id
+) = EvaluationArguments.generate_corpus(
+    evaluation_args
+)
+
+evaluation_args = EvaluationArguments.from_cli_args(
+    args=evaluation_args,
+    evaluation_corpus=evaluation_corpus,
+    evaluation_tags=evaluation_tags,
+    evaluation_step1_features=evaluation_step1_features,
+    label2id=label2id,
+)
+
+evaluation_pipeline = EvaluationPipeline(evaluation_args)
+evaluation_pipeline.run()
